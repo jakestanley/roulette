0eb54b4f24e06260bce627c64de916972688d72e
Jake Stanley <mail@jakestanley.co.uk>
2017-02-19 14:30:19 +0000

    Initial commit

---
+++ src/main/java/uk/co/jakestanley/App.java
@@ -0,0 +1,17 @@
+package uk.co.jakestanley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Checkout
+ * 
+ * @author jake.stanley
+ */
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
